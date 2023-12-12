006dc78efb6403a02887f8fe3da08676d97525c8
harshal <harshalc562@gmail.com>
2023-12-12 19:59:31 +0530

    done with cart quantity  validation

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-for(let i = 0; i < removeCartItemButtons.length; i++) {
-    var button = removeCartItemButtons[i]
-    button.addEventListener('click', function(e){
-        let buttonClick = e.target
-        buttonClick.parentElement.parentElement.remove()
-        updateCartTotal()
-    })
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready()
+}
+
+function ready(){
+    let removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for(let i = 0; i < removeCartItemButtons.length; i++) {
+    let button = removeCartItemButtons[i]
+        button.addEventListener('click', removeItemFromCart)
+    }
+    let quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for(let i = 0; i < quantityInputs.length; i++) {
+        let input = quantityInputs[i];
+        input.addEventListener('change', quantityChange)
+    }
+}
+
+function removeItemFromCart(e){
+    let buttonClick = e.target
+    buttonClick.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChange(e){
+    var input = e.target
+    console.log(e.target.value)
+    if(isNaN(input.value) || input.value <= 0){
+        input.value = 1
+    } 
+    updateCartTotal;
 }
 
 function updateCartTotal(){
@@ -20,6 +44,7 @@ function updateCartTotal(){
         // console.log(cartItemQuantity)
         let price =  parseFloat(cartItemPrice.innerText.replace('$', ''))
         let quantity = cartItemQuantity.value
+        // console.log(price, quantity)
         total = total + (price * quantity)
     }
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
